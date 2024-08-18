fca20e128869f8cb5ed6b0f55f66ea9ca17fe2ac
Haseebullah kehar <159536251+Haseebullahkehar@users.noreply.github.com>
2024-08-18 14:38:18 +0500

    Add files via upload

---
+++ BooleanDataType.py
@@ -0,0 +1,36 @@
+"""
+BOOLEAN DATA TYPE:
+    Python boolean type is one of the built-in data types provided by Python,
+    which represents one of the two values i.e. True or False.
+=> Note that we represent true as 1 and false as 0.
+For example,
+    1==1 is True whereas
+    2<1 is False.
+
+Boolean Data Type Works on Comparison Operators:
+Below are comparison operators with examples:
+"""
+
+# 1. Equal to ( == )
+print(2==2) #Output: true
+print(2==4) #Output: false
+
+# 2. not equal to ( != )
+print(2!=2) #Output: false
+print(3!=2) #Output: true
+
+# 3. Greater than ( > )
+print(2>3) #Output: false
+print(3>2) #Output: true
+
+# 4. less than ( < )
+print(4<3) #Output: false
+print(2<3) #Output: true
+
+# 5. Greater than or equal to  ( >= )
+print(2>=3) #Output: false
+print(3>=2) #Output: true
+
+# 6. less than or equal to ( <= )
+print(2<=3) #Output: true
+print(10<=5) #Output: false
---
+++ UserInput.py
@@ -0,0 +1,33 @@
+"""
+USER INPUT/String Formatting advance:
+    the program pauses to ask the user for input.
+    When the user enters some data and hits enter,
+    Python stores the data in a variable as a string.
+
+==> NOTE: Input always takes the string type values that's why
+          you can change the data type by your choice.
+
+
+Syntax:
+variable_name = input("message")
+EX:
+"""
+
+# name = input("Enter your name: ")
+
+
+
+username = input("Enter your name : ")
+password = input("Enter your password: ")
+
+print(f"User name is {username}, and user password is {password}")
+
+
+"""
+=>Taking user input and changing it into integer format
+Syntak:
+variable = int(input("Enter your number"))
+Example below:
+"""
+age = int(input("Enter your age : "))
+print(age)
